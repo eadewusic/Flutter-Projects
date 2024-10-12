d17a0e149d805259c9d1601806a23fe284765644
Eunice Adewusi <e.adewusi@alustudent.com>
2024-10-12 20:14:27 +0200

    add primary colour

---
+++ product_catalogue/lib/theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.brown, // Using brown color as primary color/theme
+      ),
+    );
+  }
+}
