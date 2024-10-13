891a98c7cebacabc8b7c830dc954ba8ccf28b716
Eunice Adewusi <e.adewusi@alustudent.com>
2024-10-13 12:20:11 +0200

    colors

--- product_catalogue/lib/theme.dart
+++ product_catalogue/lib/theme.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyTheme());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyTheme extends StatelessWidget {
+  const MyTheme({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Product Catalogue w/ Animation',
       theme: ThemeData(
         primarySwatch: Colors.brown, // Using brown color as primary color/theme
       ),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        backgroundColor: Colors.black,
+      ),
+      themeMode: ThemeMode.dark,
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
